528a86feba5ebde4432e0e3703244f5edd66ac18
Becca Harris <beccalynharris@gmail.com>
2018-04-13 00:23:14 -0500

    get twitter working and use moment to reconfigure date created_At date format

--- liri.js
+++ liri.js
@@ -1,8 +1,12 @@
+
 require("dotenv").config();
 
 var fs = require("fs");
 var keys = require("./keys.js");
-var Twitter = require('twitter');
+
+var Twitter = require('twitter')
+var Spotify = require('node-spotify-api')
+var moment = require('moment')
 
 // Need this for OMDB requests //
 var request = require("request");
@@ -14,12 +18,28 @@ var action = process.argv[2]
 switch(action) {
   case 'my-tweets':
     //Show last 20 tweets
-    var client = new Twitter(keys.twitter)
-    console.log(client)
-    // need to use count param - limit to 20.
-    // need to use q param (use @becca_kostyo - will look something like this: q=from%3ACmdr_Hadfield%20%23nasa&result_type=popular)
+    var client = new Twitter(keys.twitter);
+    var params = { q: '@becca_kostyo', count: 20 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        for (var i = 0; i < tweets.length; i++) {
+          var createdUnix = moment(new Date(tweets[i].created_at));
+          var createdAtPretty = moment(createdUnix).format("MM/DD/YYYY @ hh:mma")
+          console.log(
+            "Tweet: " + (parseInt([i]) + 1) + " of " + tweets.length + 
+            "\nTweeted on " + createdAtPretty + 
+            "\nText: " + tweets[i].text + 
+            "\n---------------------")
+        }
+      }
+    })
     break;
   case 'spotify-this-song':
+    if (process.argv[3] != null) {
+      console.log("Hello!")
+    } else {
+      console.log("blank :(")
+    }
     // Display this info about the song (argv[3] in terminal window:
       // Artist
       // Name
